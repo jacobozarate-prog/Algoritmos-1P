eadee7da52b0895ac4d68b8f683e32f48d0ec9b9
jacobozarate-prog <jacobo.zarate@uinenlinea.mx>
2025-10-10 12:37:10 -0600

    Create python.py

---
+++ python.py
@@ -0,0 +1,11 @@
+#Calcula la pendiente recta a partir de dos puntos
+x1=float(input("Ingresa x1:"))
+y1=float(input("Ingresa y1:"))
+x2=float(input("Ingresa x2:"))
+y2=float(input("Ingresa y2:"))
+ifx2==x1:
+  print("La pendienteb es indefinida (división  entre cero).")
+else:
+  m=(y2-y1)/(x2-x1)
+print("la pendiente es.",m)
+ 
